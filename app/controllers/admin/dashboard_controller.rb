6cbeb25096a12154ac380909da6502abcf8570ab
Yifan Shi <stevenls1118@msn.com>
2023-03-30 15:59:47 -0600

    fix issue for http basic auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,9 @@
 class Admin::DashboardController < ApplicationController
 
-  http_basic_authenticate_with :name => "Jungle", :password => "book"
+  http_basic_authenticate_with :name => ENV['HTTP_BASIC_AUTH_NAME'], :password => ENV['HTTP_BASIC_AUTH_PASSWORD']
 
   def show
-    
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
   
-  http_basic_authenticate_with :name => "Jungle", :password => "book"
+  http_basic_authenticate_with :name => ENV['HTTP_BASIC_AUTH_NAME'], :password => ENV['HTTP_BASIC_AUTH_PASSWORD']
 
   def index
     @products = Product.order(id: :desc).all
