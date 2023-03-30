6dec46a65e049ae30a0af119187b9a9fe5bee27b
Yifan Shi <stevenls1118@msn.com>
2023-03-30 15:56:52 -0600

    fix issue for admim http basic auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < ApplicationController
 
-  http_basic_authenticate_with :name => "Jungle", :password => "book"
+  http_basic_authenticate_with :name => ENV['HTTP_BASIC_AUTH_NAME'], :password => ENV['HTTP_BASIC_AUTH_PASSWORD']
 
   def show
     @product_count = Product.count
